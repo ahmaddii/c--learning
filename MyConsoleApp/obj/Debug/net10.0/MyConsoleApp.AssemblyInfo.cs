5a9520dad0fee1946d2361ea067011510509135a
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2026-05-16 17:22:50 +0500

    Grading Calculation Project

--- MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38bc3dd14e42f5ad3f8a942af75b9d9532e0a6e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+086482df3ff3d1eaabe43f9f412552b698b2c40b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
